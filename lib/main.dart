83dcbff3a0c8e0518c0bdedf3a3e8ee519f64c65
jackfrost13 <rohitagrl1302@gmail.com>
2018-10-24 16:11:17 +0530

    updates main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
-import 'HomePage.dart';
+//import 'loginScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
   // This widget is the root of your application.
+  @override
+  MyAppState createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -11,13 +23,70 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: new MyHomePage(),
-        appBar: AppBar(
-          title: Text('Chatapp'),
-        ),
-      ),
+      home: _handle(),
       debugShowCheckedModeBanner: false,
     );
+  }
+
+
+  Widget _handle() {
+    return StreamBuilder<FirebaseUser>(
+      stream: widget.firebaseAuth.onAuthStateChanged,
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting)
+          return widgetLoading();
+        else {
+          if (snapshot.hasData) {
+            print(snapshot.data);
+            return mainScreen();
+          } else
+            return login();
+        }
+      },
+    );
+  }
+
+  Widget widgetLoading() {
+//    waiting();
+    return Scaffold(
+      body: Center(
+        child: Text('waitingLoading'),
+      ),
+    );
+  }
+
+  Widget mainScreen() {
+    return Scaffold(
+      body: Center(
+        child: Text('mainScrren'),
+      ),
+    );
+  }
+
+  Widget login() {
+    return Container(
+      child: Center(
+        child: RaisedButton(
+          child: Text('Login'),
+          onPressed: signIn,
+        ),
+      ),
+    );
+  }
+
+  void signIn() async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    if(googleSignInAccount != null) {
+      print("null obtained");
+      GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+      if (await googleSignIn.isSignedIn())
+        print("sign in google");
+      FirebaseUser user = await widget.firebaseAuth.signInWithGoogle(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+    }
   }
 }
